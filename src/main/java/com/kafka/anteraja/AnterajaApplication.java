3c27ed1d1bdc7b9de0da17318c739316748cc554
achmad.hidayatullah <achmad.hidayatullah@sicepat.com>
2022-05-27 02:23:35 +0700

    first commit

---
+++ src/main/java/com/kafka/anteraja/AnterajaApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.anteraja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnterajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnterajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/anteraja/AnterajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.anteraja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnterajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
